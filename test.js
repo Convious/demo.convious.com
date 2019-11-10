f338d037668af13139b26983b75e722baade4780
juozas <juozapas@zabukas.com>
2019-11-10 13:26:28 +0200

    All the fixes

--- test.js
+++ test.js
@@ -10,9 +10,23 @@ function parseQuery(url) {
 
 	return params;
 }
+var parsed = parseQuery(window.location.href)
+if(parsed.url){
+	updateSrc(parsed.url);
+}
+insertLoader(parsed.slug);
 
-updateSrc(parseQuery(window.location.href).url);
-//Assign the variable to the iframe src
 function updateSrc(srcUrl) {
 	document.querySelector('.theFrame').src = srcUrl;
-};
+}
+
+function insertLoader(slug) {
+	if (!slug){
+		slug='fort_fun'
+	}
+	var script = document.createElement("script")
+	script.type = "text/javascript";
+	script.id = "convious_loader"
+	script.src = 'https://client.convious-app.com/loader.js?k='+slug;
+	document.getElementsByTagName("head")[0].appendChild(script);
+}
